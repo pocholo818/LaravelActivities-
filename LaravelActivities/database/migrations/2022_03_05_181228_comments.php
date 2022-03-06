f0cd1b216239ffc712f5df5c6f3f0833a9d6f5fa
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2022-03-06 08:30:28 +0800

    migrate and seeding

---
+++ LaravelActivities/database/migrations/2022_03_05_181228_comments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Comments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $table->id('commentId');
+        $table->string('comment');
+        $table->string('userId');
+        $table->string('postId');
+        $table->rememberToken();
+        $table->timestamps();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
